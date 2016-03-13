0b23d04a180a77b480906ba342e824f411ce3238
oparisy <olivier.parisy@gmail.com>
2016-03-13 21:54:47 +0100

    Added reflection mapping for debugging purpose

--- ibl.fs
+++ ibl.fs
@@ -1,9 +1,11 @@
 precision mediump float;
 
 uniform vec3 BaseColor;
+uniform float ReflectionPercent;
 uniform float SpecularPercent;
 uniform float DiffusePercent;
 
+uniform samplerCube ReflectionEnvMap;
 uniform samplerCube SpecularEnvMap;
 uniform samplerCube DiffuseEnvMap;
 
@@ -16,9 +18,15 @@ void main() {
     vec3 diffuseColor = vec3(textureCube(DiffuseEnvMap, normalize(Normal)));
     vec3 specularColor = vec3(textureCube(SpecularEnvMap, normalize(ReflectDir)));
     
+    // Get non-convoluted reflection
+    vec3 reflectionColor = vec3(textureCube(ReflectionEnvMap, normalize(ReflectDir)));
+    
     // Add lighting to base color and mix
     vec3 color = mix(BaseColor, diffuseColor*BaseColor, DiffusePercent);
     color = mix(color, specularColor + color, SpecularPercent);
+    
+    // Mix with mirror-like reflection
+    color = mix(color, reflectionColor, ReflectionPercent);
 
     gl_FragColor = vec4(color, 1.0);
 }
